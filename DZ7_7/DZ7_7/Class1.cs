3fc34f7d64527c0fedc391630d3a6ef215671dcd
Sapsan <semya_s@mail.ru>
2023-01-16 22:35:04 +0300

    Проект решения

---
+++ DZ7_7/DZ7_7/Class1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ7_7
+{
+    public class ArrayWorker
+    {
+        /// <summary>
+        /// Функция определения длины элемента
+        /// </summary>
+        /// <param name="array">Массив, в котором определяется длина элементов</param>
+        /// <param name="length">Значение длины, заданное переменной</param>
+        /// <returns></returns>
+        public int CheckArray(string[] array, int length) // Функция определения длины элемента
+        {
+            int result = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Length <= length)
+                    result++;
+            }
+            return result;
+        }
+        /// <summary>
+        /// Функция заполнения нового массива значениями
+        /// </summary>
+        /// <param name="Array">Массив с данными, из которого они переносятся в новый</param>
+        /// <param name="newArray">Новый массив</param>
+        /// <param name="lengthLimit">Значение лимита длины</param>
+        public void NewArray(string[] Array, string[] newArray, int lengthLimit)// функция заполнения нового массива значениями
+        {
+            int temp = 0;
+            for (int i = 0; i < Array.Length; i++)
+            {
+                if (Array[i].Length <= lengthLimit)
+                {
+                    newArray[temp] = Array[i];
+                    temp++;
+                }
+            }
+        }
+        /// <summary>
+        /// Функция вывода массива
+        /// </summary>
+        /// <param name="array">Массив для вывода</param>
+        /// <returns>Элементы массива </returns>
+        public string PrintArray(string[] array)
+        {
+            string result = string.Empty;
+            result = "[ ";
+            for (int i = 0; i < array.Length; i++)
+            {
+                result += $"{array[i],1}";
+                if (i < array.Length - 1) result += ", ";
+            }
+            result += " ]";
+            return result;
+        }
+    }
+}
---
+++ DZ7_7/DZ7_7/Program.cs
@@ -0,0 +1,38 @@
+﻿using DZ7_7;
+using System;
+ArrayWorker worker = new ArrayWorker();
+
+Console.Clear();
+Console.WriteLine("     Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк," +
+    "\r\n длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры," +
+    "\r\n либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями," +
+    "\r\n лучше обойтись исключительно массивами." +
+    "\r\n   Примеры:\r\n" +
+    "   [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]\r\n\r\n" +
+    "   [“1234”, “1567”, “-2”, “computer science”] → [“-2”]\r\n\r\n" +
+    "   [“Russia”, “Denmark”, “Kazan”] → []");
+Console.WriteLine();
+
+string[] Array = new string[] { };
+Console.Write("Введите количество элементов массива: ");
+int n = int.Parse(Console.ReadLine());
+
+Array = new string[n];
+for (int i = 0; i < Array.Length; i++)
+{
+    Console.Write($" Введите симиволы {i + 1}-ого элемента: ");
+    Array[i] = Console.ReadLine();
+}
+Console.WriteLine();
+Console.WriteLine("Введённый массив:");
+Console.WriteLine($"{worker.PrintArray(Array)}");
+Console.WriteLine();
+
+int length = 3;
+int numbers = worker.CheckArray(Array, length);// переменная с элементом массива, меньше либо равного 3 символам 
+
+string[] newArray = new string[numbers]; // создаем новый  с количеством, равным количеству коротких элементов
+worker.NewArray(Array, newArray, length);
+
+Console.WriteLine("Строки массива,  длина которых меньше, либо равна 3 символам:");
+Console.WriteLine($"{worker.PrintArray(newArray)}");
